66c1625b5e455b668a4d4afea3aadee3595abb37
mert <mert.inal@gmail.com>
2024-04-23 09:11:32 +0200

    first commit

---
+++ src/main/java/com/myths/artifact_myths/ArtifactMythsApplication.java
@@ -0,0 +1,13 @@
+package com.myths.artifact_myths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactMythsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactMythsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myths/artifact_myths/ArtifactMythsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myths.artifact_myths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactMythsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
