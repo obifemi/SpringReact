323b645971e624615be5f7ff2ab34117ce8a88b3
mert <mert.inal@gmail.com>
2024-04-25 00:18:49 +0200

    user entity

---
+++ src/main/java/com/myths/artifact_myths/user/User.java
@@ -0,0 +1,76 @@
+package com.myths.artifact_myths.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity()
+@Table(name = "users")
+public class User {
+    private String username;
+    private String email;
+    private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, String role, String status, long id) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.id = id;
+   
+    }
+
+    public String getusername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+ 
+
+    public void setusername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
