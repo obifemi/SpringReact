4faf8bdde57a841a4c1802a2de903f13580667b4
mert <mert.inal@gmail.com>
2024-04-25 00:18:26 +0200

    controller using service

--- src/main/java/com/myths/artifact_myths/user/UserController.java
+++ src/main/java/com/myths/artifact_myths/user/UserController.java
@@ -1,17 +1,21 @@
 package com.myths.artifact_myths.user;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 @RestController
 public class UserController {
 
+@Autowired 
+UserService userService;
    
     @PostMapping("api/v1/user")
-    void createUser() {
-        System.out.println("User created");
+    void createUser(@RequestBody User user) {
+        userService.createUser(user);
     }
     
 }
