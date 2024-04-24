2e38fdd342695f6d9289f19a6798b8122aff4ad0
mert <mert.inal@gmail.com>
2024-04-25 00:18:36 +0200

    repo

---
+++ src/main/java/com/myths/artifact_myths/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.myths.artifact_myths.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
