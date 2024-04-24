33a7294ae2c723cfb88bd94a0941f192eac846d7
mert <mert.inal@gmail.com>
2024-04-24 10:59:54 +0200

    signup page and main.jsx edit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
+// import './index.css'
 import SignUp from './pages/SignUp/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -3,7 +3,31 @@ import React from 'react';
 const SignUp = () => {
     return (
         <div>
-           signup page
+            <h1>SignUp</h1>
+            <div>
+            <label htmlFor="username">Username</label>
+            <input id='username'/>
+            </div>
+            
+            <div>
+            <label htmlFor="email">E-Mail</label>
+            <input id='email'/>
+            </div>
+
+            <div>
+            <label htmlFor="password">Password</label>
+            <input id='password'/>
+            </div>
+
+            <div>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input id='confirmPassword'/>
+            </div>
+
+            <button>Sign Up</button>
+            
+           
+
         </div>
     );
 };
