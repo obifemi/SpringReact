aa44a0322785e9ddee8bb52741ee74f486c01a27
mert <mert.inal@gmail.com>
2024-04-27 21:52:29 +0200

    disabled security for initiation

--- src/main/java/com/myths/artifact_myths/ArtifactMythsApplication.java
+++ src/main/java/com/myths/artifact_myths/ArtifactMythsApplication.java
@@ -2,12 +2,16 @@ package com.myths.artifact_myths;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+   SecurityAutoConfiguration.class,
+    org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+    )
 public class ArtifactMythsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtifactMythsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArtifactMythsApplication.class, args);
+    }
 
 }
