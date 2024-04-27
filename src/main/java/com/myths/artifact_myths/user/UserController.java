492b7434c0d652a33ff821018a39300d6dd9a7ae
mert <mert.inal@gmail.com>
2024-04-27 21:52:58 +0200

    controller is updated with generic message and api/v1/user endopint

--- src/main/java/com/myths/artifact_myths/user/UserController.java
+++ src/main/java/com/myths/artifact_myths/user/UserController.java
@@ -1,6 +1,9 @@
 package com.myths.artifact_myths.user;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myths.artifact_myths.shared.GenericMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +17,9 @@ public class UserController {
 UserService userService;
    
     @PostMapping("api/v1/user")
-    void createUser(@RequestBody User user) {
+     GenericMessage createUser(@RequestBody User user) {
         userService.createUser(user);
+        return new GenericMessage("User created successfully");
     }
     
 }
